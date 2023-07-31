edf4b6a6b66a5718fafb11a53cd0a0cd3abb1ac0
AvielSheto <avielsheto@gmail.com>
2023-07-31 21:13:53 +0300

    Creating Prisma modules

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Post } from "@nestjs/common"
-import { AuthService } from "./auth.service";
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    signup() {
-        return this.authService.signup();
-    }
+  @Post('signup')
+  signup(@Body() dto: any) {
+    console.log(
+        dto,
+    );
+    return this.authService.signup();
+  }
 
-    @Post('signin')
-    signin() {
-        return this.authService.signin();
-    }
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
 }
