1201540319d587db927ca447e6ade70c7e2e9955
AvielSheto <avielsheto@gmail.com>
2023-08-03 23:05:47 +0300

    Automate postgres restart & prisma migrations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,7 +12,6 @@ export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
-
     try {
       const user = await this.prisma.user.create({
         data: {
@@ -39,7 +38,30 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { msg: 'I have signed in' };
+  async signin(dto: AuthDto) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+    if (!user)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+
+    delete user.hash;
+    return user;
   }
 }
