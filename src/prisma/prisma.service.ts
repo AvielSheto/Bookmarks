e5f99ea619e734698dd577fefdaae51bba63723a
AvielSheto <avielsheto@gmail.com>
2023-08-15 21:10:20 +0300

    Adding e2e tests with pactumJs

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,13 +3,13 @@ import { PrismaClient } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(config: ConfigService){
-        super({
-            datasources: {
-                db: {
-                    url: config.get('DATABASE_URL')
-                },
-            },
-        });
-    }
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
 }
